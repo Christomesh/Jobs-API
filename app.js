f471c5fd9493065d17ad02ee1b6886b41dbb454f
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-07-18 09:45:18 +0100

    finished initial template setup

---
+++ app.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+
+const app = express();
+
+// error handler -> middleware
+const errorHandlerMiddleware = require("./middlewares/error-handler");
+const notFoundMiddleware = require("./middlewares/not-found");
+
+app.use(express.json());
+
+
+// routes
+app.get("/", (req, res)=>{
+    res.send("Job api");
+});
+
+
+
+
+// use middlewares
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () =>{
+    try {
+        app.listen(port, ()=> (
+            console.log(`Server is listening on port ${port}...`)
+        ));
+
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+start();

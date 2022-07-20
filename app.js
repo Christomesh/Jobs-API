bd7a988e420ab61c4cdb04186724f11e5134431d
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-07-20 15:48:32 +0100

    added extra security layer with use of npm packages

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 require("dotenv").config();
 require("express-async-errors");
-const express = require("express");
 
+// extra security packages
+const helmet = require("helmet")
+const cors = require("cors")
+const xss = require("xss-clean")
+const rateLimiter = require("express-rate-limit")
+
+
+const express = require("express");
 const app = express();
 
 //DB
@@ -16,9 +23,21 @@ const jobsRouter = require("./routes/job")
 const errorHandlerMiddleware = require("./middlewares/error-handler");
 const notFoundMiddleware = require("./middlewares/not-found");
 
-app.use(express.json());
+
 
 //extra package
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 
 // routes
 app.use("/api/v1/auth", authRouter)
